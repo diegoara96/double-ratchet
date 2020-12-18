eb3e839c544847395f05e13b05c3b0a97beff25d
diegoara96 <diegoaraujonovoa@gmail.com>
2020-12-18 16:38:47 +0100

    mqtt persistence

--- publi.py
+++ publi.py
@@ -6,14 +6,14 @@ broker = '52.210.173.185'
 port =    1883
 topic = "/python/mqtt"
 # generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
+client_id = "publi"
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
-    client = mqtt_client.Client(client_id)
+    client = mqtt_client.Client(client_id,clean_session=False)
     client.username_pw_set(username="sinf",password="snif20")
     client.on_connect = on_connect
     client.connect(broker, port)
@@ -23,7 +23,7 @@ def publish(client):
     while True:
         time.sleep(1)
         msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
+        result = client.publish(topic, msg,1)
         # result: [0, 1]
         status = result[0]
         if status == 0:
--- subs.py
+++ subs.py
@@ -5,14 +5,14 @@ broker = '52.210.173.185'
 port =  1883
 topic = "/python/mqtt"
 # generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 100)}'
+client_id = 'sub'
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)    
-    client = mqtt_client.Client(client_id)
+    client = mqtt_client.Client(client_id,clean_session=False)
     client.username_pw_set(username="sinf",password="snif20")
     client.on_connect = on_connect
     client.connect(broker, port)
@@ -20,7 +20,7 @@ def connect_mqtt() -> mqtt_client:
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")    
-    client.subscribe(topic)
+    client.subscribe(topic,1)
     client.on_message = on_message
 def run():
     client = connect_mqtt()
