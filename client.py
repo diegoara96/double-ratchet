9fb9f95027dc27435e6c997ec0e357b3db26db43
diegoara96 <diegoaraujonovoa@gmail.com>
2020-12-20 16:07:15 +0100

    ping pong

--- client.py
+++ client.py
@@ -1,40 +1,86 @@
-import mqtt
+from paho.mqtt import client as mqtt_client
+import threading
 from doublerachet import Rachet
 import doublerachet
+import json
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
+
+broker='52.210.173.185'
+port=1883
+Qos=0
+result_available = threading.Event()
+
+
 
 def main():
-
     name_from = input("FROM: ")
     name_to = input("TO: ")
-    server=  mqtt.mqtt()
-    server.connect_mqtt(name_from,"sinf","snif20")
-
-  
+    rachet=Rachet()
+    server=connect_mqtt(name_from,"sinf","snif20")
+    hilo1 = threading.Thread(target=subscribe,args=(server,name_from,rachet,name_to))
+    hilo1.start()
 
 
     while True:
-        msg=input("MESSAGE: ")
-        server.publish(msg,name_to)
+        message=input("MESSAGE: ")
+        if(rachet.DHr==0):
+            msg=json.dumps({"DH":None,"C":None})
+            publish(server,msg,name_to)
+            result_available.wait()
+            print("clave publica")
+        message=doublerachet.RatchetEncrypt(rachet,message)
+        publish(server,message,name_to)
     
 
 
+def subscribe(client,name_from,rachet:Rachet,name_to):
+    def on_connect(client, userdata, flags, rc):
+        if rc != 0:
+            print("Failed to connect, return code %d\n", rc)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    def on_message(client, userdata, msg):
+        response=json.loads(msg.payload.decode())
+        if response["C"]==None:
+            if response["DH"]!=None:
+                doublerachet.setDHr(rachet,response)  
+                result_available.set()
+            else:
+                public = doublerachet.getPublicDH(rachet=rachet)
+                param = doublerachet.getParametersDH(rachet=rachet)
+                message=json.dumps({"DH":public,"PN":rachet.PN, "N":rachet.Ns,"C":None,"PAM":param})
+                publish(client,message,name_to)
+                print("enviando parametros")
+        else:            
+            messaege=doublerachet.RatchetDecrypt(rachet,response)
+            print(messaege)
+       
+    client.subscribe(name_from,Qos)
+    client.on_message = on_message
+    client.loop_forever()
 
-def rachet():
-    alice = Rachet()
-    pk_a = alice.public_key
-    bob = Rachet()
-    pk_b=bob.public_key
-    dh_out=alice.dhstep(pk_b)
-    rk,ck=doublerachet.kdf_rk(alice.rk,dh_out)
-    mk,ck=doublerachet.kdf_ck(ck)
-    ct=doublerachet.encrypt("test",mk)
-    print(ct)
-    msg=doublerachet.decrypt(ct,mk)
-    print(msg)
 
+def publish(client,msg,name_to):
+    result = client.publish(name_to, msg,Qos)
+    status = result[0]
+    if status != 0:
+        print(f"Failed to send message to {name_to} {status}")
     
+
+def connect_mqtt(name_from,username=None,password=None) -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc != 0:
+            print("Failed to connect, return code %d\n", rc)
+        
+    client = mqtt_client.Client(name_from,clean_session=True)
+    if username:
+        client.username_pw_set(username,password)
+    client.on_connect = on_connect
+    client.connect(broker,port)
+    return client
+
 
 
 if __name__ == '__main__':
-    #main()
-    rachet()
+    main()
+    
--- doublerachet.py
+++ doublerachet.py
@@ -3,17 +3,28 @@ from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_parameters
+import json
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat, ParameterFormat
+import base64
 
 class Rachet():
     def __init__(self):
-        self.parameters = dh.generate_parameters(generator=2, key_size=2048,backend=default_backend())
-        self.private_key = self.parameters.generate_private_key()
-        self.public_key = self.private_key.public_key()
-        self.rk = b'V\xb6\xb1\x92&!Q~\xcd\x7f\xb1\xf5\xee\xefJ\xeb'
+        self.parameters = dh.generate_parameters(generator=2, key_size=512,backend=default_backend())
+        self.DHs = self.parameters.generate_private_key()
+        self.DHr =0
+        self.PN=0
+        self.Ns=0
+        self.Nr=0
+        self.CKr=0
+        self.CKs=0
+        self.RK = b'V\xb6\xb1\x92&!Q~\xcd\x7f\xb1\xf5\xee\xefJ\xeb'
+        
 
-    def dhstep(self,peer_public_key):
-        shared_key = self.private_key.exchange(peer_public_key)
-        return shared_key
+
+def dhstep(private_key,peer_public_key):
+    shared_key = private_key.exchange(peer_public_key)
+    return shared_key
 
 def kdf_rk(rk,dh_out):
     derived_key = HKDF(algorithm=hashes.SHA256(),length=64,salt=rk,info=b'kdf_rk',backend=default_backend()).derive(dh_out)
@@ -25,20 +36,61 @@ def kdf_ck(ck):
     h.update(b"0x01")
     d=h.copy()
     d.update(b'0x02')
-    mk=d.finalize()
-    ck=h.finalize()
-    return (mk,ck)
+    mk=h.finalize()
+    ck=d.finalize()
+    return (ck,mk)
     
+
+def DHRatchet(state:Rachet, DHr):
+    state.PN = state.Ns                          
+    state.Ns = 0
+    state.Nr = 0
+    state.DHr = DHr
+    state.RK, state.CKr = kdf_rk(state.RK, dhstep(state.DHs, state.DHr))
+    state.DHs = state.parameters.generate_private_key()
+    state.RK, state.CKs = kdf_rk(state.RK, dhstep(state.DHs, state.DHr))
+
+
+def RatchetEncrypt(rachet, plaintext):
+    rachet.CKs, mk = kdf_ck(rachet.CKs)
+    response =  json.dumps({"DH":getPublicDH(rachet), "PN":rachet.PN, "N":rachet.Ns,"C":encrypt(plaintext,mk)})
+    rachet.Ns += 1
+    return response
+
+def RatchetDecrypt(rachet, response):
+    DHr=load_pem_public_key(bytes(response["DH"],encoding='utf-8'), backend=default_backend())
+    if DHr != rachet.DHr:           
+        DHRatchet(rachet, DHr)          
+    rachet.CKr, mk = kdf_ck(rachet.CKr)
+    rachet.Nr += 1
+    return decrypt(response["C"],mk)
+
 
 def encrypt(msg:str,key):
     aesgcm = AESGCM(key)
     nonce=b'12345'
     ct = aesgcm.encrypt(nonce, msg.encode(),associated_data=None)
-    return ct
+    return str(base64.urlsafe_b64encode(ct),encoding='utf-8')
 
 
 def decrypt(ct,key):
+    
     aesgcm = AESGCM(key)
     nonce=b'12345'
-    msg=aesgcm.decrypt(nonce, ct,associated_data=None)
+    msg=aesgcm.decrypt(nonce, base64.urlsafe_b64decode(bytes(ct,encoding='utf-8')),associated_data=None)
     return msg.decode()
+
+def setDHr(rachet:Rachet,response):
+    rachet.DHr=load_pem_public_key(bytes(response["DH"],encoding='utf-8'), backend=default_backend())
+    setParameterDH(rachet,response)
+    rachet.DHs=rachet.parameters.generate_private_key()
+    rachet.RK,rachet.CKs= kdf_rk(rachet.RK,dhstep(rachet.DHs,rachet.DHr))
+
+def getPublicDH(rachet:Rachet):
+   return str(rachet.DHs.public_key().public_bytes(Encoding.PEM,PublicFormat.SubjectPublicKeyInfo),encoding='utf-8')
+
+def getParametersDH(rachet:Rachet):
+    return str(rachet.parameters.parameter_bytes(Encoding.PEM,ParameterFormat.PKCS3),encoding='utf-8')
+
+def setParameterDH(rachet:Rachet,response):
+    rachet.parameters=load_pem_parameters(bytes(response["PAM"],encoding='utf-8'), backend=default_backend())
